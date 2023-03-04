6d7c76f5f0dab7cb1a4aef54f6702ad2ae834652
金文辉 <rish0705@163.com>
2023-03-04 18:47:26 +0800

    first commit

---
+++ src/main/java/com/rish/rish_login/RishLoginApplication.java
@@ -0,0 +1,13 @@
+package com.rish.rish_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RishLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RishLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rish/rish_login/RishLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rish.rish_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RishLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
